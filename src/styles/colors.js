36d196b32704e2cb6ea2228adbe0e593a3fa3ef0
“sgiori11” <stephaniegiori9@gmail.com>
2023-02-19 14:41:08 +0100

    pls work

---
+++ src/styles/colors.js
@@ -0,0 +1,21 @@
+// colors.js
+
+module.exports = {
+    // Font colors
+    fontPrimary: '#333333',
+    fontSecondary: '#666666',
+    fontTertiary: '#999999',
+  
+    // Background colors
+    bgPrimary: '#FFFFFF',
+    bgSecondary: '#F7F7F7',
+    bgTertiary: '#EAEAEA',
+  
+    // Button colors
+    btnPrimary: '#0070F3',
+    btnSecondary: '#6B7280',
+    btnTertiary: '#9CA3AF',
+    
+    // ... more color variables ...
+  };
+  
