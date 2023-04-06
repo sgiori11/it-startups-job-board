2b58bf9e9480f7f75c95cb2e1d8e33628c0245af
“sgiori11” <stephaniegiori9@gmail.com>
2023-04-06 17:15:16 +0200

    added dummy components for about, startups

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+function About() {
+    return (
+      <div>
+        <h1>About Page</h1>
+      </div>
+    );
+  }
+  
+  export default About; // Add this line to export the About component as the default export
+  
---
+++ src/pages/startups.js
@@ -0,0 +1,10 @@
+function Startups() {
+    return (
+      <div>
+        <h1>Startups Page</h1>
+      </div>
+    );
+  }
+  
+  export default Startups; // Add this line to export the About component as the default export
+  
