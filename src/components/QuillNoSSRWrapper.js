93aba33edab41c6ea28fb0d77b1a4fee2ecad029
“sgiori11” <stephaniegiori9@gmail.com>
2023-04-06 19:22:48 +0200

    fallback: blocking, not false

--- src/components/QuillNoSSRWrapper.js
+++
@@ -1,8 +0,0 @@
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-
-const QuillNoSSRWrapper = (props) => {
-  return <ReactQuill {...props} />;
-};
-
-export default QuillNoSSRWrapper;
--- src/pages/jobs/[id].js
+++ src/pages/jobs/[id].js
@@ -78,7 +78,7 @@ export async function getStaticPaths() {
 
     return {
         paths,
-        fallback: false,
+        fallback: 'blocking',
     };
   }
 
