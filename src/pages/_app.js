523cd02d0a5c596f1bb645f7f2eccb1181070bd4
“sgiori11” <stephaniegiori9@gmail.com>
2023-04-19 23:52:05 +0200

    auth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { useState } from 'react'
-import '@/styles/globals.css'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { useState, useEffect } from 'react';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
 // Create a new supabase browser client on every first render.
   const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+
+ 
 
   return (
     <SessionContextProvider
