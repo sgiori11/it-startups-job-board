3a2ee600b83c328944cd1184defb631086e641da
sgiori11 <stephaniegiori9@gmail.com>
2024-02-12 21:17:46 +0100

    testing git email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
         <footer className={styles.footer}>
             <div className={styles.container}>
                 <div className={styles.content}>
-                  <p>Built in Milano by 
+                  <p>Built in Milan by 
                      <Link href="https://sgiori.xyz" className={styles.Twitterlink}> Steph</Link>
                   </p>
                   <Link className={styles.privacy} href="/privacy.html" target="_blank" rel="noopener noreferrer">Privacy Policy</Link>
