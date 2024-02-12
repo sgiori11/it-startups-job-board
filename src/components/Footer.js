34efcc03115cb1974629d1d444b42bc0b3838d4a
SG <stephsstar@gmail.com>
2024-02-12 21:07:33 +0100

    added portfolio to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
             <div className={styles.container}>
                 <div className={styles.content}>
                   <p>Built in Milan by 
-                     <Link href="https://www.layoverinitaly.com/about/" className={styles.Twitterlink}> Steph</Link>
+                     <Link href="https://sgiori.xyz" className={styles.Twitterlink}> Steph</Link>
                   </p>
                   <Link className={styles.privacy} href="/privacy.html" target="_blank" rel="noopener noreferrer">Privacy Policy</Link>
                 </div>
